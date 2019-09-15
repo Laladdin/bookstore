aeed314e065b4b6241cb11b4ba9fd1d2a18a6b66
Laladdin <laura.laukkanen@myy.haaga-helia.fi>
2019-09-15 13:33:31 +0300

    tokat lisäykset

--- bookstore/src/main/java/trainingProject/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/trainingProject/bookstore/BookstoreApplication.java
@@ -1,13 +1,40 @@
 package trainingProject.bookstore;
 
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+import trainingProject.bookstore.domain.Book;
+import trainingProject.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+	
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);  // uusi loggeriattribuutti
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository bookRepository) { 
+		return (args) -> {
+			log.info("save a couple of books");
+			bookRepository.save(new Book("Learning Python", "Lutz, Mark & Asher, David", 1999, "1-56592-464-9", 19.90));
+			bookRepository.save(new Book("Oracle - Tietokannan tehokas hallinta", "Hakkarainen, Anssi", 2011, "978-952-220-485", 29.95));	
+			
+			log.info("fetch all books");
+			for (Book book : bookRepository.findAll()) {
+				log.info(book.toString());
+			}
+
+		};
 	}
 
 }
--- bookstore/src/main/java/trainingProject/bookstore/domain/Book.java
+++ bookstore/src/main/java/trainingProject/bookstore/domain/Book.java
@@ -1,13 +1,34 @@
 package trainingProject.bookstore.domain;
 
-public class Book {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Book {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
 	private String title;
 	private String author;
 	private int year;
 	private String isbn;
 	private double price;
 	
+	
+	
+	public Book() {
+		super();
+		this.id = null;
+		this.title = null;
+		this.author = null;
+		this.year = 0;
+		this.isbn = null;
+		this.price = 0;
+		
+	}
+
 	public Book(String title, String author, int year, String isbn, double price) {
 		super();
 		this.title = title;
@@ -15,6 +36,26 @@ public class Book {
 		this.year = year;
 		this.isbn = isbn;
 		this.price = price;
+	}
+	
+	
+
+	public Book(Long id, String title, String author, int year, String isbn, double price) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public String getTitle() {
@@ -59,9 +100,10 @@ public class Book {
 
 	@Override
 	public String toString() {
-		return "Book [title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn + ", price=" + price
-				+ "]";
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn
+				+ ", price=" + price + "]";
 	}
+	
 	
 	
 	
